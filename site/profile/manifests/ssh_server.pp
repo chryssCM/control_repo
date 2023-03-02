ea578348a6495a6f8bb840ecb9fdae7c47123c05
chryssCM <126565632+chryssCM@users.noreply.github.com>
2023-03-02 21:19:06 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
            ensure => present,
            user   => 'root',
            type   => 'ssh-rsa',
-           key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCz5uQwPNfDy33HT3VQYqo+0HxsMRQgBddIKoHmIWMh7k3idWRwFkkOXzWG8QbUvXiOrysswckguX3DqWBNY/kyk3bW9mzUumnmJiMBMoZhkbVBbJ4h+ZkM7WcGRUXfFBetwmB9Oq7CQ+1sS70KkG+TIbW2tXaGRrxGi+wQ8G7X8sa2IbligbU2Ju7mt79mv9Z2Vgqm8O19a7Qxk0xfvWDffV1Otthbm7NVoNbjkQKpiYL/oKqeEEA8q1Bc4FxiNxRgDCs8Hk3JLvOHd3MDitrTySHUP1JP5Zw6MhifCGNzCxx94SgG7GT0E6wdJyK/SAzjzKp1VbogYoKQLZhfYxyn root@master.puppet.vm',
+           key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCz5uQwPNfDy33HT3VQYqo+0HxsMRQgBddIKoHmIWMh7k3idWRwFkkOXzWG8QbUvXiOrysswckguX3DqWBNY/kyk3bW9mzUumnmJiMBMoZhkbVBbJ4h+ZkM7WcGRUXfFBetwmB9Oq7CQ+1sS70KkG+TIbW2tXaGRrxGi+wQ8G7X8sa2IbligbU2Ju7mt79mv9Z2Vgqm8O19a7Qxk0xfvWDffV1Otthbm7NVoNbjkQKpiYL/oKqeEEA8q1Bc4FxiNxRgDCs8Hk3JLvOHd3MDitrTySHUP1JP5Zw6MhifCGNzCxx94SgG7GT0E6wdJyK/SAzjzKp1VbogYoKQLZhfYxyn',
     }          
 }    

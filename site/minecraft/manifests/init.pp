a805e8c16d51ac9b9546164d1af0ebbda0c2fe61
chryssCM <126565632+chryssCM@users.noreply.github.com>
2023-03-06 18:18:42 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -24,7 +24,7 @@ class minecraft (
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     #source => 'puppet:///modules/minecraft/minecraft.service',
-    content => epp('minecraft/minecraft.service','{ install_dir => $install_dir, }')
+    content => epp('minecraft/minecraft.service', { install_dir => $install_dir, })
   }
   service {'minecraft':
     ensure => running,
